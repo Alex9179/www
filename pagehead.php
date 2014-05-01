18c79ed2c9b81a054212ff90042d75f0ae3daa9f
Alex9179 <muse91@aol.com>
2014-05-01 14:29:49 +0100

    PHPUnit tests completed

--- pagehead.php
+++ pagehead.php
@@ -7,7 +7,6 @@
   <meta name="viewport" content="width=device-width">
   <meta name="mobile-web-app-capable" content="yes">
   <link rel="icon" sizes="196x196" href="/icon.png">
-  <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
--- tests.php
+++ tests.php
@@ -1,19 +1,97 @@
 <?php
 class functiontest extends PHPUnit_Framework_TestCase
 {
-    // ...
+    //Check the correct graphic is showing and functional
 
-    public function testCanBeNegated()
+    public function testAssertweathergraph()
     {
         // Arrange
-        $a = new Money(1);
+        $W = 1;
 
         // Act
-        $b = $a->negate();
+        $result =  Weathergraph($W);
+
 
         // Assert
-        $this->assertEquals(-1, $b->getAmount());
+        $this->assertEquals  ('images/sunny_day.png', $result);
     }
 
-    // ...
+    //Check the correct weather type is returned and functional
+
+    public function testAssertweathertype()
+    {
+        // Arrange
+        $W = 8;
+
+        // Act
+        $result =  Weathertype($W);
+
+
+        // Assert
+        $this->assertEquals  ('<td class="warning">Overcast</td>', $result);
+    }
+
+ //Check the correct visibility type is returned and functional
+
+    public function testAssertvisibility()
+    {
+        // Arrange
+        $V = "PO";
+
+        // Act
+        $result =  Visibility($V);
+
+
+        // Assert
+        $this->assertEquals  ('<td class="warning" data-toggle="tooltip" data-placement="right" title="1-4km">Poor</td>', $result);
+    }
+
+
+ //Check the correct visibility graphic is shown and functional
+
+    public function testAssertvisgraph()
+    {
+        // Arrange
+        $V = "EX";
+
+        // Act
+        $result =  Visgraph($V);
+
+
+        // Assert
+        $this->assertEquals  ('images/sunny_day.png', $result);
+    }
+
+ //Check the correct Temperature type is shown and functional
+
+    public function testAssertTempgraph()
+    {
+        // Arrange
+        $T = 21;
+
+        // Act
+        $result =  Tempgraph($T);
+
+
+        // Assert
+        $this->assertEquals  ('images/3.png', $result);
+    }
+
+//Check the correct wind direction is shown
+
+    public function testAssertwindgraph()
+    {
+        // Arrange
+        $D = "NNE";
+
+        // Act
+        $result =  Windgraph($D);
+
+
+        // Assert
+        $this->assertEquals  ('images/wind_north_east.png', $result);
+    }
+
+
+
 }
